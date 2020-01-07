2654ed05c760a824e12dd873a64f03092bcfb142
brianthomasross <brianthomasross@users.noreply.github.com>
2020-01-06 23:25:40 -0500

    add basic api BTR
    - basic flask app with dummy data return

---
+++ heroku/tester.py
@@ -0,0 +1,20 @@
+import json
+import requests
+
+# if you want to test local
+url = "http://localhost:9000"
+
+# deployed
+
+##
+
+val = {'post_body': "Example of a reddit post body for testing"}
+
+
+
+# you'll get a 200 response if the keys align
+# and something bad if the keys don't align.
+if __name__ == '__main__':
+    r_success = requests.post(url, data=json.dumps(val))
+    print(
+        f"Request responded: {r_success}.\nResponse was\n{r_success.json()}")
