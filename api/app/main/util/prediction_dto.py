609030937685936a3183289acba64c3a20c25ae5
brianthomasross <brianthomasross@users.noreply.github.com>
2020-01-08 11:39:47 -0500

    Update prediction_dto.py

--- api/app/main/util/prediction_dto.py
+++ api/app/main/util/prediction_dto.py
@@ -3,6 +3,6 @@ from flask_restplus import Namespace, fields
 class PredictionDto:
     api = Namespace('predict', description='prediction related operations')
     preds = api.model('Submission', {
-        'return_count': fields.String(required=True, description='numbers of predictions to return'),
+        'return_count': fields.Integer(required=True, description='numbers of predictions to return'),
         'submission_text': fields.String(required=True, description='body of the post being used to make prediction')
     })
