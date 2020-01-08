2a966c55e33149e2dad825dbc5122d48df90cf6a
brianthomasross <brianthomasross@users.noreply.github.com>
2020-01-08 09:24:34 -0500

    API v0.2
    -Flask-RESTplus API
    -DB implementation

---
+++ api/app/main/service/prediction_service.py
@@ -0,0 +1,27 @@
+from traceback import format_exc
+import pandas as pd
+
+def make_prediction(data):
+    """
+    Model will go here
+    """
+    try:
+        dummy = {"name":['askreddit', 'funny', 'learnpython', 'datascience', 'learnprogramming'],
+                    "proba":[0.12332, 0.116558, 0.78855, 0.63144, 0.21144]}
+        n = data['return_count']
+        text = data['submission_text']
+        n=4
+        response_object = {'predictions': pd.DataFrame(dummy).head(n).to_dict(orient='records')}
+
+        return response_object, 201
+
+    except Exception:
+        tb = format_exc()
+        response_object = {
+            'status': 'fail',
+            'message': tb,
+        }
+
+        return response_object, 409
+
+
